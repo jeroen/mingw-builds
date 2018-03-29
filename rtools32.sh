43edd305603620192540c7dcae96b9c8518435a7
Jeroen Ooms <jeroenooms@gmail.com>
2018-03-29 12:55:16 +0000

    early test run of gcc 8

---
+++ rtools32.sh
@@ -0,0 +1,1 @@
+./build --mode=gcc-trunk --rt-version=v5 --arch=i686 --exceptions=dwarf --static-gcc --threads=posix --enable-languages=c,c++,fortran 
---
+++ rtools64.sh
@@ -0,0 +1,1 @@
+./build --mode=gcc-trunk --rt-version=v5 --arch=x86_64 --exceptions=seh --static-gcc --threads=posix --enable-languages=c,c++,fortran
