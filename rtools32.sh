8fed027a2700740f1823028d0e6791d1570106a7
Jeroen Ooms <jeroenooms@gmail.com>
2018-05-02 13:59:34 +0200

    add gcc-8.1.0

--- rtools32.sh
+++ rtools32.sh
@@ -1,1 +1,1 @@
-./build --mode=gcc-trunk --rt-version=v5 --arch=i686 --exceptions=dwarf --static-gcc --no-multilib --no-extras --threads=posix --enable-languages=c,c++,fortran
+./build --mode=gcc-8.1.0 --rt-version=v5 --arch=i686 --exceptions=dwarf --static-gcc --no-multilib --no-extras --threads=posix --enable-languages=c,c++,fortran
--- rtools64.sh
+++ rtools64.sh
@@ -1,1 +1,1 @@
-./build --mode=gcc-trunk --rt-version=v5 --arch=x86_64 --exceptions=seh --static-gcc --no-multilib --no-extras --threads=posix --enable-languages=c,c++,fortran
+./build --mode=gcc-8.1.0 --rt-version=v5 --arch=x86_64 --exceptions=seh --static-gcc --no-multilib --no-extras --threads=posix --enable-languages=c,c++,fortran
